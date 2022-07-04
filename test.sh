71ed1801087bf44cefaa9ffa9b6926b50bc39d79
keishiba <keishiba1218@gmail.com>
2022-07-04 08:16:30 +0000

    trivial change

--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@
 # PYTHONPATH="./:${PYTHONPATH}" CUDA_VISIBLE_DEVICES=3,2 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4321 codes/train.py -opt options/train/train_FiveK_11.yml --launcher pytorch
 # PYTHONPATH="./:${PYTHONPATH}" CUDA_VISIBLE_DEVICES=3,2 python -m torch.distributed.launch --nproc_per_node=2 --master_port=1234 codes/train.py -opt options/train/LPTN/train_FiveK.yml --launcher pytorch
 
-# PYTHONPATH="./:${PYTHONPATH}" CUDA_VISIBLE_DEVICES=3 python codes/test.py -opt options/test/LPTT/test_FiveK.yml
-# PYTHONPATH="./:${PYTHONPATH}" CUDA_VISIBLE_DEVICES=1 python codes/test_speed.py -opt options/test/LPTN/test_speed_FiveK.yml
+PYTHONPATH="./:${PYTHONPATH}" CUDA_VISIBLE_DEVICES=3 python codes/test.py -opt options/test/LPTN/test_FiveK.yml
+PYTHONPATH="./:${PYTHONPATH}" CUDA_VISIBLE_DEVICES=3 python codes/test_speed.py -opt options/test/LPTT/test_speed_FiveK.yml
 
 # CUDA_VISIBLE_DEVICES=3 python inference.py
